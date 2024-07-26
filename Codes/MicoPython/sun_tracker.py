33e510afffa1a9a22d64386c9ff9e357d2004267
selimgayretli <113355046+selimgayretli@users.noreply.github.com>
2024-07-26 16:41:17 +0300

    Update sun_tracker.py

--- Codes/MicoPython/sun_tracker.py
+++ Codes/MicoPython/sun_tracker.py
@@ -1,4 +1,5 @@
 ##########Libraries##########
+from time import sleep
 from machine import Pin, PWM, UART, Timer, ADC
 import time, utime
 from berrybot import TB6612, HCSR04
@@ -11,72 +12,45 @@ MOTOR_PWM_A_PIN = 15
 MOTOR_PWM_B_PIN = 21
 LDR_L_PIN = 29
 LDR_R_PIN = 28
-rowPins= [7, 11, 12, 13, 17]
-colPins = [3, 2, 16, 19, 18]
+
 #############Variables####################
 LDR_THRESHOLD = 250
-ledRow = 0
-ledArrayBuffer = bytearray(5)
-#############Led Matrix###################
-sunny =     [0x11,0x0E,0x0A,0x0E,0x11]
+LDR_TOLERANCE = 5000
+
 ##########Pin Initialization##########
 motor = TB6612(MOTOR_A1_PIN, MOTOR_A2_PIN, MOTOR_B1_PIN, MOTOR_B2_PIN, MOTOR_PWM_A_PIN, MOTOR_PWM_B_PIN)
-sensor = HCSR04(trigger_pin=12, echo_pin=11, echo_timeout_us=10000)
+sensor = HCSR04(trigger_pin=8, echo_pin=9, echo_timeout_us=10000)
 ldr_left = ADC(LDR_L_PIN)
 ldr_right = ADC(LDR_R_PIN)
-##########Function Declaration##########
-def drawScreen(buffer):
-    for i in range(0, 5):
-        ledArrayBuffer[i] = buffer[i]
-		
-def setColumns(b):
-    machine.Pin(colPins[0]).value((~b >> 0) & 0x01)
-    machine.Pin(colPins[1]).value((~b >> 1) & 0x01)
-    machine.Pin(colPins[2]).value((~b >> 2) & 0x01)
-    machine.Pin(colPins[3]).value((~b >> 3) & 0x01)
-    machine.Pin(colPins[4]).value((~b >> 4) & 0x01)
-		
-def setLedMatrix():
-    for i in range(0, 5):
-        machine.Pin(colPins[i], machine.Pin.OUT)
-        machine.Pin(rowPins[i], machine.Pin.OUT)
-		
-def tick(timer):
-    global ledRow
-    drawScreen(smile)
-    if (ledRow == 5):
-        ledRow = 0
-
-    setColumns(ledArrayBuffer[ledRow])
-    machine.Pin(rowPins[ledRow]).value(1)
-    time.sleep_ms(1)
-    machine.Pin(rowPins[ledRow]).value(0)
-    ledRow = ledRow + 1
-
-setLedMatrix()
-myTimer = Timer(-1)
-myTimer.init(period=5, mode=Timer.PERIODIC, callback=tick)
 
 while True:
-    drawScreen(sunny)
+   
     LDR_L = ldr_left.read_u16()
     LDR_R = ldr_right.read_u16()
-    #print(leftSensorValue)
-    #print(rightSensorValue)
+    #print(LDR_L)
+    #print(LDR_R)
     sleep(0.02)
     distance = sensor.distance_cm()
     #print(distance)
     sleep(0.02)
+    
 
     if LDR_L >= LDR_THRESHOLD and LDR_R >= LDR_THRESHOLD:
-        if (LDR_R - LDR_L) >= LDR_TOLERANCE:
-            motor.right(150)
+        if distance < 10:
+            motor.stop()
+            sleep(0.02)
+            motor.left(80*650)
+            sleep(0.5)
+            motor.stop()
+        elif (LDR_R - LDR_L) >= LDR_TOLERANCE:
+            motor.right(80*650)
         elif (LDR_L - LDR_R) >= LDR_TOLERANCE:
-            motor.left(150)
+            motor.left(80*650)   
+        elif ( LDR_L >= 10000 and LDR_R >= 10000):
+            motor.forward(100*650)
         else:
-            if distance >= 15
-                motor.forward(255)
-            else
-                motor.stop()
+            motor.stop()
     else:
         motor.stop()
+
+
