c00bb55b6168e088e7f5b700e09aa2e4cb8e02e5
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-07-29 14:06:35 +0300

    Update sumo.ino

--- Codes/Arduino/sumo.ino
+++ Codes/Arduino/sumo.ino
@@ -1,7 +1,6 @@
 // Libraries
 #include <Wire.h>
 #include <stdio.h>
-#include "RPi_Pico_TimerInterrupt.h"
 
 // Pin Defination
 #define MOTOR_B1 22
@@ -18,18 +17,13 @@
 #define TRACKER_THRESHOLD 1200
 #define HighSpeed 255
 
-// Led Matrix
-int tracker[5] =   {0x07,0x04,0x1F,0x04,0x07};
-
 // Variable
 long duration;
 int distance;
 int leftSensor = 0;
 int rightSensor = 0;
+int counter = 0;
 unsigned long reverseTime = 0;
-
-int rowPins[5] = {7, 11, 12, 13, 17}; //Row LedMatrix Pins
-int colPins[5] = {18, 19, 16, 2, 3};  //Col LedMatrix Pins
 
 void attachMotor()
 {
@@ -109,40 +103,6 @@ void hcsr() {
   //Serial.println("cm");
 }
 
-volatile int ledRow = 0;
-volatile int prevLedRow = 4;
-volatile byte ledArrayBuffer[5];
-
-void drawScreen(int buffer[]){
-  for (int i = 0; i < 5; i++) {
-      ledArrayBuffer[i] = buffer[i];
-  }
-}
-
-void setColumns(byte b) {
-    digitalWrite(colPins[0], (~b >> 0) & 0x01); 
-    digitalWrite(colPins[1], (~b >> 1) & 0x01); 
-    digitalWrite(colPins[2], (~b >> 2) & 0x01); 
-    digitalWrite(colPins[3], (~b >> 3) & 0x01); 
-    digitalWrite(colPins[4], (~b >> 4) & 0x01); 
-}
-
-bool TimerHandler0(struct repeating_timer *t)
-{
-  (void) t; 
-  if (ledRow == 5)
-    ledRow = 0;
-  setColumns(ledArrayBuffer[ledRow]); 
-  digitalWrite(rowPins[ledRow], HIGH);
-  digitalWrite(rowPins[prevLedRow], LOW);
-  prevLedRow = ledRow;
-  ledRow++;
-
-  return true;
-}
-
-RPI_PICO_Timer ITimer0(0);
-
 void setup() {
   Serial.begin(115200);
   Wire.begin();
@@ -154,17 +114,9 @@ void setup() {
 
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  
-  if (ITimer0.attachInterruptInterval(3 * 1000, TimerHandler0))
-  {
-    Serial.print(F("Starting ITimer0 OK, millis() = ")); Serial.println(millis());
-  }
-  else
-    Serial.println(F("Can't set ITimer0. Select another freq. or timer"));
 }
 
 void loop(){
-  drawScreen(tracker);
   hcsr();
   leftSensor = analogRead(LEFT_SENSOR);
   rightSensor = analogRead(RIGHT_SENSOR);
@@ -180,20 +132,30 @@ void loop(){
       Backward(HighSpeed);
       delay(500);
     }
-    else if ((leftSensor < TRACKER_THRESHOLD) && (rightSensor < TRACKER_THRESHOLD))
+    else if ((leftSensor < TRACKER_THRESHOLD) && (rightSensor < TRACKER_THRESHOLD)){
       Forward(HighSpeed);
+      delay(500);
+    }
     else
       Stop();
   }
   else{
     if ((leftSensor >= TRACKER_THRESHOLD) || (rightSensor >= TRACKER_THRESHOLD)){
         Backward(HighSpeed);
-        delay(500);
+        delay(100);
     }
     else if ((leftSensor < TRACKER_THRESHOLD) && (rightSensor < TRACKER_THRESHOLD)){
-        Left(HighSpeed);
-        delay(100);
-        Stop();
+        counter += 1;
+        if (counter == 3){
+          Forward(HighSpeed);
+          delay(100);
+          counter = 0;
+        }
+        else{
+          Left(HighSpeed);
+          delay(100);
+          Stop();
+        }
     }else
         Stop();
   }
