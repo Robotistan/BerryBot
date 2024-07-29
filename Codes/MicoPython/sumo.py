7c18819ca314494f37101c576e295b9e1ee08c02
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-07-29 14:07:45 +0300

    Update sumo.py

--- Codes/MicoPython/sumo.py
+++ Codes/MicoPython/sumo.py
@@ -14,7 +14,7 @@ RIGHT_TRACKER = 27
 #############Variables####################
 MotorSpeed = 50000
 threshold = 60000
-counter=0
+counter = 0
 ##########Pin Initialization##########
 motor = TB6612(MOTOR_A1_PIN, MOTOR_A2_PIN, MOTOR_B1_PIN, MOTOR_B2_PIN, MOTOR_PWM_A_PIN, MOTOR_PWM_B_PIN)
 sensor = HCSR04(trigger_pin=8, echo_pin=9, echo_timeout_us=10000)
@@ -24,14 +24,13 @@ rightSensor = ADC(Pin(RIGHT_TRACKER))
 while True:
     distance = sensor.distance_cm()
     #print(distance)
-   
 
+    leftSensorValue = leftSensor.read_u16()
+    rightSensorValue = rightSensor.read_u16()
+    #print(leftSensorValue)
+    #print(rightSensorValue)
+    sleep(0.02)
     if distance <= 15:
-        leftSensorValue = leftSensor.read_u16()
-        rightSensorValue = rightSensor.read_u16()
-        #print(leftSensorValue)
-        #print(rightSensorValue)
-        sleep(0.02)
         if leftSensorValue >= threshold or rightSensorValue >= threshold:
             motor.backward(MotorSpeed)
             sleep(0.5)
@@ -41,24 +40,18 @@ while True:
         else:
             motor.stop()
     else:
-        leftSensorValue = leftSensor.read_u16()
-        rightSensorValue = rightSensor.read_u16()
-        #print(leftSensorValue)
-        #print(rightSensorValue)
-        sleep(0.02)
         if leftSensorValue >= threshold or rightSensorValue >= threshold:
             motor.backward(MotorSpeed)
             sleep(0.1)
         elif leftSensorValue < threshold and rightSensorValue < threshold:
-            counter=counter+1
-            if counter==3:
+            counter = counter + 1
+            if counter == 3:
                 motor.forward(MotorSpeed)
                 sleep(0.1)
-                counter=0
+                counter = 0
             else: 
                 motor.left(MotorSpeed)
                 sleep(0.1)
                 motor.stop()
         else:
             motor.stop()
-
