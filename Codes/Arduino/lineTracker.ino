39ff7d12378f852eaa9d484d9a7e98ee12e27ea1
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-11-12 11:25:55 +0300

    Update lineTracker.ino

--- Codes/Arduino/lineTracker.ino
+++ Codes/Arduino/lineTracker.ino
@@ -1,19 +1,16 @@
-// Libraries
-#include <Wire.h>
-#include <stdio.h>
-
 // Pin Defination
+#define TRIG_PIN 8
+#define ECHO_PIN 9
+#define MODE_BUTTON 10
+#define BUZZER_PIN 14
+#define PWM_A 15
+#define PWM_B 21
 #define MOTOR_B1 22
 #define MOTOR_B2 23
 #define MOTOR_A2 24
 #define MOTOR_A1 25
-#define PWM_A 15
-#define PWM_B 21
 #define LEFT_SENSOR 26
 #define RIGHT_SENSOR 27
-#define MODE_BUTTON 10
-
-#define TRACKER_THRESHOLD 900
 
 #define STOP  0
 #define FWD   1
@@ -21,12 +18,14 @@
 #define RIGHT 3
 #define LEFT  4
 
-// Variable
+#define TRACKER_THRESHOLD 500
+
+// Variables
 int leftSensor = 0;
 int rightSensor = 0;
+unsigned long reverseTime = 0;
 uint8_t directionStt = STOP;
 uint8_t oldDirection = STOP;
-unsigned long reverseTime = 0;
 
 void attachMotor()
 {
@@ -38,60 +37,55 @@ void attachMotor()
   pinMode(PWM_B, OUTPUT);
 }
 
-void Forward(int speed)
-{
-  digitalWrite(MOTOR_A1, HIGH);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, HIGH);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
+void move (int direction, int speed){
+  if (direction == FWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
 
-void Backward(int speed)
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == BWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
 
-void Left(int speed)
-{
-  digitalWrite(MOTOR_A1, HIGH);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == RIGHT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
 
-void Right(int speed)
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, HIGH);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == LEFT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == STOP){
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, 0);
 
-void Stop()
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, 0);
-  analogWrite(PWM_B, 0);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, LOW);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
-  delay(100);
 
   attachMotor();
   pinMode(LEFT_SENSOR, INPUT);
@@ -99,58 +93,42 @@ void setup() {
   pinMode(MODE_BUTTON, OUTPUT);
 
   while (digitalRead(MODE_BUTTON)==0){
-    Stop();
-    Serial.println(digitalRead(MODE_BUTTON));
+    move(STOP,0);
   }
-
 }
 
 void loop(){
   leftSensor = analogRead(LEFT_SENSOR);
   rightSensor = analogRead(RIGHT_SENSOR);
-  
   /*
   Serial.print("left:");
   Serial.println(leftSensor);
   Serial.print("  right:");
   Serial.println(rightSensor);
   */
-
   if (leftSensor >= TRACKER_THRESHOLD && rightSensor >= TRACKER_THRESHOLD) {
     directionStt = FWD;
-    //Serial.println("FWD");
   } 
   else if (leftSensor < TRACKER_THRESHOLD && rightSensor > TRACKER_THRESHOLD) {
-    directionStt = RIGHT;
-    //Serial.println("RIGHT");
+    directionStt = LEFT;
   } 
   else if (leftSensor > TRACKER_THRESHOLD && rightSensor < TRACKER_THRESHOLD) {
-    directionStt = LEFT;
-    //Serial.println("LEFT");
+    directionStt = RIGHT;
   } 
   else if (leftSensor < TRACKER_THRESHOLD && rightSensor < TRACKER_THRESHOLD && directionStt != STOP) {
     directionStt = BWD;
-    //Serial.println("BWD");
   }
 
   if (directionStt != oldDirection) {
     oldDirection = directionStt;
     if (directionStt == FWD)
-      Forward(220);
+      move(FWD, 220);
     else if (directionStt == RIGHT)
-      Right(220);
+      move(RIGHT, 220);
     else if (directionStt == LEFT)
-      Left(220);
+      move(LEFT, 220);
     else if (directionStt == BWD) {
-      //Backward(140);
       reverseTime = millis();
     }
-    /*else if (directionStt == STOP)
-      Stop();
-    */
   }
-
-  if (directionStt == BWD && millis() - reverseTime > 300)
-    directionStt = STOP;
-
 }
