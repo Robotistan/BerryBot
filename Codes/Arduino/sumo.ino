0b8c504bb29afe57fe9c24f58f9bdee79cfb0c2e
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-11-12 11:28:40 +0300

    Update sumo.ino

--- Codes/Arduino/sumo.ino
+++ Codes/Arduino/sumo.ino
@@ -1,30 +1,30 @@
-// Libraries
-#include <Wire.h>
-#include <stdio.h>
-
 // Pin Defination
+#define TRIG_PIN 8
+#define ECHO_PIN 9
+#define MODE_BUTTON 10
+#define BUZZER_PIN 14
+#define PWM_A 15
+#define PWM_B 21
 #define MOTOR_B1 22
 #define MOTOR_B2 23
 #define MOTOR_A2 24
 #define MOTOR_A1 25
-#define PWM_A 15
-#define PWM_B 21
 #define LEFT_SENSOR 26
 #define RIGHT_SENSOR 27
-#define TRIG_PIN 8
-#define ECHO_PIN 9
-#define MODE_BUTTON 10
 
-#define TRACKER_THRESHOLD 1000
-#define HighSpeed 255
+#define STOP  0
+#define FWD   1
+#define BWD   2
+#define RIGHT 3
+#define LEFT  4
 
-// Variable
+#define TRACKER_THRESHOLD 500
+
+// Variables
 long duration;
-int distance;
 int leftSensor = 0;
 int rightSensor = 0;
 int counter = 0;
-unsigned long reverseTime = 0;
 
 void attachMotor()
 {
@@ -36,57 +36,8 @@ void attachMotor()
   pinMode(PWM_B, OUTPUT);
 }
 
-void Forward(int speed)
-{
-  digitalWrite(MOTOR_A1, HIGH);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, HIGH);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Backward(int speed)
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Left(int speed)
-{
-  digitalWrite(MOTOR_A1, HIGH);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Right(int speed)
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, HIGH);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Stop()
-{
-  digitalWrite(MOTOR_A1, LOW);
-  digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, 0);
-  analogWrite(PWM_B, 0);
-}
-
-void hcsr() {
+long hcsr() {
+  long distance;
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
 
@@ -100,73 +51,104 @@ void hcsr() {
 
   distance = (duration / 2) / 29.1;
 
-  //Serial.print(cm);
-  //Serial.println("cm");
+  return distance;
+}
+
+void move (int direction, int speed){
+  if (direction == FWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == BWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == RIGHT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == LEFT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == STOP){
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, 0);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, LOW);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
-  delay(100);
 
   attachMotor();
   pinMode(LEFT_SENSOR, INPUT);
   pinMode(RIGHT_SENSOR, INPUT);
-
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
   pinMode(MODE_BUTTON, OUTPUT);
 
   while (digitalRead(MODE_BUTTON)==0){
-    Stop();
-    Serial.println(digitalRead(MODE_BUTTON));
+    move(STOP,0);
   }
-
 }
 
 void loop(){
-  hcsr();
+  long distance = hcsr();
   leftSensor = analogRead(LEFT_SENSOR);
   rightSensor = analogRead(RIGHT_SENSOR);
-  
 
-  /*
-  Serial.print("left:");
-  Serial.println(leftSensor);
-  Serial.print("  right:");
-  Serial.println(rightSensor);
-  */
-  delay(20);
-  if (distance <= 15) {
+  if (distance <= 22) {
     if ((leftSensor >= TRACKER_THRESHOLD) || (rightSensor >= TRACKER_THRESHOLD)){
-      Backward(HighSpeed);
-      delay(100);
+      move(BWD,255);
+      delay(500);
     }
     else if ((leftSensor < TRACKER_THRESHOLD) && (rightSensor < TRACKER_THRESHOLD)){
-      Forward(HighSpeed);
+      move(FWD,255);
       delay(100);
     }
     else
-      Stop();
+      move(STOP,0);
   }
   else{
     if ((leftSensor >= TRACKER_THRESHOLD) || (rightSensor >= TRACKER_THRESHOLD)){
-        Backward(HighSpeed);
-        delay(100);
+        move(BWD,255);
+        delay(200);
     }
     else if ((leftSensor < TRACKER_THRESHOLD) && (rightSensor < TRACKER_THRESHOLD)){
         counter += 1;
         if (counter == 3){
-          Forward(HighSpeed);
+          move(FWD,180);
           delay(100);
           counter = 0;
         }
         else{
-          Left(HighSpeed);
+          move(LEFT,180);
           delay(100);
-          Stop();
+          move(STOP,0);
         }
     }else
-        Stop();
+      move(STOP,0);
   }
 }
