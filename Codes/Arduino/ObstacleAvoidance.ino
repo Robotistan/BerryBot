1622a4f56678d064593efc21b06800dc01cb5bbd
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-11-12 11:18:10 +0300

    Update ObstacleAvoidance.ino

--- Codes/Arduino/ObstacleAvoidance.ino
+++ Codes/Arduino/ObstacleAvoidance.ino
@@ -1,92 +1,84 @@
-#include <Wire.h>
-#include <stdio.h>
-
 // Pin Defination
-#define TX_PIN 0
-#define RX_PIN 1
-#define NEOPIXEL_PIN 6
 #define TRIG_PIN 8
 #define ECHO_PIN 9
 #define MODE_BUTTON 10
 #define BUZZER_PIN 14
 #define PWM_A 15
-#define IR_PIN 20
 #define PWM_B 21
-#define INPUT_B1 22
-#define INPUT_B2 23
-#define INPUT_A2 24
-#define INPUT_A1 25
-#define LEFT_SENSOR 26
-#define RIGHT_SENSOR 27
-#define LDR_R_PIN 28
-#define LDR_L_PIN 29
+#define MOTOR_B1 22
+#define MOTOR_B2 23
+#define MOTOR_A2 24
+#define MOTOR_A1 25
 
-// Variable
+#define STOP  0
+#define FWD   1
+#define BWD   2
+#define RIGHT 3
+#define LEFT  4
+
+// Variables
 long duration;
-int distance;
 int left_counter=0;
 
 void attachMotor()
 {
-  pinMode(INPUT_A1, OUTPUT);
-  pinMode(INPUT_A2, OUTPUT);
-  pinMode(INPUT_B1, OUTPUT);
-  pinMode(INPUT_B2, OUTPUT);
+  pinMode(MOTOR_A1, OUTPUT);
+  pinMode(MOTOR_A2, OUTPUT);
+  pinMode(MOTOR_B1, OUTPUT);
+  pinMode(MOTOR_B2, OUTPUT);
   pinMode(PWM_A, OUTPUT);
   pinMode(PWM_B, OUTPUT);
 }
 
-void Forward(int speed)
-{
-  digitalWrite(INPUT_A1, HIGH);
-  digitalWrite(INPUT_A2, LOW);
-  digitalWrite(INPUT_B1, HIGH);
-  digitalWrite(INPUT_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
+void move (int direction, int speed){
+  if (direction == FWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == BWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == RIGHT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == LEFT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == STOP){
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, 0);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, LOW);
+  }
 }
 
-void Backward(int speed)
-{
-  digitalWrite(INPUT_A1, LOW);
-  digitalWrite(INPUT_A2, HIGH);
-  digitalWrite(INPUT_B1, LOW);
-  digitalWrite(INPUT_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Left(int speed)
-{
-  digitalWrite(INPUT_A1, LOW);
-  digitalWrite(INPUT_A2, HIGH);
-  digitalWrite(INPUT_B1, HIGH);
-  digitalWrite(INPUT_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Right(int speed)
-{
-  digitalWrite(INPUT_A1, HIGH);
-  digitalWrite(INPUT_A2, LOW);
-  digitalWrite(INPUT_B1, LOW);
-  digitalWrite(INPUT_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Stop()
-{
-  digitalWrite(INPUT_A1, LOW);
-  digitalWrite(INPUT_A2, LOW);
-  digitalWrite(INPUT_B1, LOW);
-  digitalWrite(INPUT_B2, LOW);
-  analogWrite(PWM_A, 0);
-  analogWrite(PWM_B, 0);
-}
-
-void hcsr() {
+long hcsr() {
+  long distance;
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
 
@@ -100,49 +92,36 @@ void hcsr() {
 
   distance = (duration / 2) / 29.1;
 
-  //Serial.print(cm);
-  //Serial.println("cm");
+  return distance;
 }
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
-  delay(1999);
-
-  pinMode(BUZZER_PIN, OUTPUT);
 
   attachMotor();
-  pinMode(LDR_L_PIN, INPUT_PULLUP);
-  pinMode(LDR_R_PIN, INPUT_PULLUP);
-
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-
-  pinMode(LEFT_SENSOR, INPUT);
-  pinMode(RIGHT_SENSOR, INPUT);
   pinMode(MODE_BUTTON, OUTPUT);
 
   while (digitalRead(MODE_BUTTON)==0){
-    Stop();
+    move(STOP,0);
   }
-
 }
 
 void loop(){
-  
-  hcsr();
-  int cm=distance;
-  if(cm>12){
-    Forward(255);
+  long distance = hcsr();
+
+  if(distance > 12){
+    move(FWD, 255);
   }
   else{
-    Stop();
+    move(STOP,0);
     delay(500);
-    Backward(200);
+    move(BWD, 200);
     delay(100);
-    Stop();
+    move(STOP, 0);
     delay(200);
-    Left(200);
+    move(LEFT, 200);
     if(left_counter==0){
       delay(500);
       left_counter++;
@@ -151,8 +130,7 @@ void loop(){
       delay(800);
       left_counter=0;
     }
-    
-    Stop();
+    move(STOP,0);
     delay(200);
   }
 }
