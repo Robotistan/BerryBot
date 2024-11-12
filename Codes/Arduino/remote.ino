924526f92f682ef1abdc8655c0ba717a44ec082b
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-11-12 11:33:51 +0300

    Update remote.ino

--- Codes/Arduino/remote.ino
+++ Codes/Arduino/remote.ino
@@ -1,14 +1,22 @@
 // Libraries
 #include <IRremote.h>
-
 // Pin Defination
-#define IR_PIN 20
+#define TX_PIN 0
+#define RX_PIN 1
+#define MODE_BUTTON 10
+#define BUZZER_PIN 14
 #define PWM_A 15
 #define PWM_B 21
 #define MOTOR_B1 22
 #define MOTOR_B2 23
 #define MOTOR_A2 24
 #define MOTOR_A1 25
+
+#define STOP  0
+#define FWD   1
+#define BWD   2
+#define RIGHT 3
+#define LEFT  4
 
 //IR Button Numbers
 #define number_1 69
@@ -39,86 +47,103 @@ void attachMotor()
   pinMode(PWM_B, OUTPUT);
 }
 
-void Forward(int speed)
-{
-	digitalWrite(MOTOR_A1, HIGH);
-	digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, HIGH);
-	digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
+void move (int direction, int speed){
+  if (direction == FWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == BWD){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == RIGHT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+
+    digitalWrite(MOTOR_A1, HIGH);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, HIGH);
+  }
+  else if (direction == LEFT){
+    analogWrite(PWM_A, speed);
+    analogWrite(PWM_B, speed);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, HIGH);
+    digitalWrite(MOTOR_B1, HIGH);
+    digitalWrite(MOTOR_B2, LOW);
+  }
+  else if (direction == STOP){
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, 0);
+
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, LOW);
+  }
 }
 
-void Backward(int speed)
-{
-	digitalWrite(MOTOR_A1, LOW);
-	digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, LOW);
-	digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Right(int speed)
-{
-	digitalWrite(MOTOR_A1, HIGH);
-	digitalWrite(MOTOR_A2, LOW);
-	digitalWrite(MOTOR_B1, LOW);
-  digitalWrite(MOTOR_B2, HIGH);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Left(int speed)
-{
-  digitalWrite(MOTOR_A1, LOW);
-	digitalWrite(MOTOR_A2, HIGH);
-  digitalWrite(MOTOR_B1, HIGH);
-	digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, speed);
-  analogWrite(PWM_B, speed);
-}
-
-void Stop()
-{
-	digitalWrite(MOTOR_A1, LOW);
-	digitalWrite(MOTOR_A2, LOW);
-  digitalWrite(MOTOR_B1, LOW);
-	digitalWrite(MOTOR_B2, LOW);
-  analogWrite(PWM_A, 0);
-  analogWrite(PWM_B, 0);
+void berry_horn() {
+  for(int i=0; i<50; i++){
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(2);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(2);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
+
   attachMotor();
-  IrReceiver.begin(IR_PIN);
+  pinMode(MODE_BUTTON, OUTPUT);
+
+  while (digitalRead(MODE_BUTTON)==0){
+    move(STOP,0);
+  }
 }
 
-void loop() {
-  Stop();
+void loop(){
   if (IrReceiver.decode()) {
     IrReceiver.resume();
     if(IrReceiver.decodedIRData.command != 0){
-      Serial.println(IrReceiver.decodedIRData.command);
+      //Serial.println(IrReceiver.decodedIRData.command);
       if(IrReceiver.decodedIRData.command == button_up){  //Forward
-        Forward(255);
+        move(FWD,255);
         delay(500);
+        move(STOP,0);
       }
       else if(IrReceiver.decodedIRData.command == button_down){  //Backward
-        Backward(255);
+        move(BWD,255);
         delay(500);
+        move(STOP,0);
       }
       else if(IrReceiver.decodedIRData.command == button_left){  //Left
-        Left(255);
+        move(LEFT,255);
         delay(130);
+        move(STOP,0);
       }
       else if(IrReceiver.decodedIRData.command == button_right){  //Right
-        Right(255);
+        move(RIGHT,255);
         delay(130);
+        move(STOP,0);
+      }
+      else if(IrReceiver.decodedIRData.command == number_1){  //Buzzer
+        berry_horn();
       }
       else{ //Stop
-        Stop();
+        move(STOP,0);
       }
       IrReceiver.decodedIRData.command = 0;
     }
