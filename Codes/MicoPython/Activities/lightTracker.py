662cda04bd84e2fa09e284a7960e7d3eb2995b26
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-12-17 13:44:38 +0300

    Update lightTracker.py

--- Codes/MicoPython/Activities/lightTracker.py
+++ Codes/MicoPython/Activities/lightTracker.py
@@ -17,6 +17,9 @@ MODE_BUTTON = 10
 LDR_THRESHOLD = 250
 LDR_TOLERANCE = 5000
 
+Max_Speed = 65000
+Mid_Speed = 50000
+Low_Speed = 47000
 ##########Pin Initialization##########
 motor = TB6612(MOTOR_A1_PIN, MOTOR_A2_PIN, MOTOR_B1_PIN, MOTOR_B2_PIN, MOTOR_PWM_A_PIN, MOTOR_PWM_B_PIN)
 sensor = HCSR04(trigger_pin=8, echo_pin=9, echo_timeout_us=10000)
@@ -27,7 +30,6 @@ while (push_button.value()==0):
     motor.stop()
     
 while True:
-   
     LDR_L = ldr_left.read_u16()
     LDR_R = ldr_right.read_u16()
     #print(LDR_L)
@@ -37,21 +39,20 @@ while True:
     #print(distance)
     sleep(0.02)
     
-
     if LDR_L >= LDR_THRESHOLD and LDR_R >= LDR_THRESHOLD:
         if distance < 10:
-            motor.stop()
+            motor.move(STOP,0)
             sleep(0.02)
-            motor.left(80*650)
+            motor.move(LEFT, Mid_Speed)
             sleep(0.5)
-            motor.stop()
+            motor.move(STOP, 0)
         elif (LDR_R - LDR_L) >= LDR_TOLERANCE:
-            motor.right(80*650)
+            motor.move(RIGHT, Mid_Speed)
         elif (LDR_L - LDR_R) >= LDR_TOLERANCE:
-            motor.left(80*650)   
+            motor.move(LEFT, Mid_Speed)   
         elif ( LDR_L >= 10000 and LDR_R >= 10000):
-            motor.forward(100*650)
+            motor.move(FWD, Max_Speed)
         else:
-            motor.stop()
+            motor.move(STOP, 0)
     else:
-        motor.stop()
+        motor.move(STOP, 0)
